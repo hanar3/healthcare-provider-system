66418140e3c3309a3a3ac367af3ecd7812f0e93f
hanar3 <vh.morenodasilva@gmail.com>
2026-01-04 20:44:21 -0300

    removing forgot password (for now)

--- apps/web/src/components/login-form.tsx
+++ apps/web/src/components/login-form.tsx
@@ -68,12 +68,12 @@ export function LoginForm({
 					<>
 						<div className="flex items-center">
 							<FieldLabel htmlFor="password">Password</FieldLabel>
-							<a
-								href="#"
-								className="ml-auto text-sm underline-offset-4 hover:underline"
-							>
-								Forgot your password?
-							</a>
+							{/* <a */}
+							{/* 	href="#" */}
+							{/* 	className="ml-auto text-sm underline-offset-4 hover:underline" */}
+							{/* > */}
+							{/* 	Forgot your password? */}
+							{/* </a> */}
 						</div>
 						<Input
 							id={field.name}
